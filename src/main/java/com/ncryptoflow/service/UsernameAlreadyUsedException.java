f1d94130dc276b6ef653f7d864d3a0b5e3ec64b1
Miłosz Godlewski <milosz.godlewski@gmail.com>
2023-10-11 16:55:24 +0200

    Initial version of ncryptoflow generated by generator-jhipster@8.0.0-beta.2

---
+++ src/main/java/com/ncryptoflow/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.ncryptoflow.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
---
+++ src/main/java/com/ncryptoflow/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer visual models.
+ */
+package com.ncryptoflow.web.rest.vm;
